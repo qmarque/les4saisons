46c18abc85beaeb4a6856c4cde84097d7b59721c
Quentin Marque <quentinm2000@hotmail.fr>
2022-02-17 21:43:25 +0100

    test: 💍 Added unit tests on Modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,7 +1,7 @@
+import React from "react";
 import { useState } from "react";
 
 import "./Modal.css";
-import Season from "./Season";
 
 function Modal({ label, children }) {
   const [open, setOpen] = useState(false);
